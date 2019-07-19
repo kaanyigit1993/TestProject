f3478f1e551abf69171806b834f4ffcd16063644
KAANYIGIT <kaan.yigit.tu@gmail.com>
2019-07-19 03:12:53 +0300

    first

---
+++ intro/src/main/java/com/kaanyigit/intro/App.java
@@ -0,0 +1,13 @@
+package com.kaanyigit.intro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
